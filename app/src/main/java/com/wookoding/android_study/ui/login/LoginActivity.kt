3e6458f8c4d6f9b4ff0292b977685bb962ffe01f
Wookjin Jang <jin090130@webcash.co.kr>
2023-01-06 17:56:08 +0900

    liveData 개발중,,

--- app/src/main/java/com/wookoding/android_study/ui/login/LoginActivity.kt
+++ app/src/main/java/com/wookoding/android_study/ui/login/LoginActivity.kt
@@ -1,7 +1,10 @@
 package com.wookoding.android_study.ui.login
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.wookoding.android_study.databinding.ActivityLoginBinding
 
@@ -22,6 +25,36 @@ class LoginActivity : AppCompatActivity() {
         val checkBox = binding.chkAutoLogin
 
         loginViewModel = ViewModelProvider(this).get(LoginViewModel::class.java)
+
+        loginViewModel.loginFormState.observe(this@LoginActivity, Observer {
+            val loginState = it ?: return@Observer
+
+            // disable login button unless both username / password is valid
+            btnLogin.isEnabled = loginState.isDataValid
+
+            if (loginState.usernameError != null) {
+                idEdit.error = getString(loginState.usernameError)
+            }
+            if (loginState.passwordError != null) {
+                pwdEdit.error = getString(loginState.passwordError)
+            }
+        })
+
+        idEdit.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                loginViewModel.loginDataChanged(
+                    idEdit.text.toString(),
+                    pwdEdit.text.toString()
+                )
+            }
+
+        })
 
         btnLogin.setOnClickListener {
             loginViewModel.requestLogin(idEdit.text.toString(), pwdEdit.text.toString(), checkBox.isChecked)
--- app/src/main/java/com/wookoding/android_study/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/wookoding/android_study/ui/login/LoginViewModel.kt
@@ -1,7 +1,10 @@
 package com.wookoding.android_study.ui.login
 
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.wookoding.android_study.R
 
 /**
  * LoginActivity ViewModel
@@ -10,7 +13,22 @@ import androidx.lifecycle.ViewModel
  **/
 class LoginViewModel : ViewModel() {
 
+    private val _loginForm = MutableLiveData<LoginFormState>()
+    val loginFormState: LiveData<LoginFormState> = _loginForm
+
     fun requestLogin(uid: String, upwd: String, isAutoLogin: Boolean) {
         Log.d("LoginViewModel", "uid : $uid upwd : $upwd isAutoLogin : $isAutoLogin")
+    }
+
+    fun loginDataChanged(uid: String, upwd: String) {
+        if (uid.isEmpty()) {
+            _loginForm.value = LoginFormState(usernameError = R.string.invalid_id)
+        }
+//        else if (upwd.isEmpty()) {
+//            _loginForm.value = LoginFormState(passwordError = R.string.invalid_pwd)
+//        }
+        else {
+            _loginForm.value = LoginFormState(isDataValid = true)
+        }
     }
 }
