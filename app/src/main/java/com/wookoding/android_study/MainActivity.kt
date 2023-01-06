321acfda2ee1cbf0ba886c6b9dfb3de7e27e9eb8
Wookjin Jang <jin090130@webcash.co.kr>
2023-01-06 18:01:01 +0900

    로그인 화면 예제

--- app/src/main/java/com/wookoding/android_study/MainActivity.kt
+++ app/src/main/java/com/wookoding/android_study/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.wookoding.android_study
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.wookoding.android_study.ui.login.LoginActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<TextView>(R.id.tv_test).setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
     }
 }
